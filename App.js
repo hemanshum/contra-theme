11b225baa49a6019d202bd4df7a2b5254aeb6af6
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2020-08-12 07:12:08 +0530

    Added fonts

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
 import OnbordingScreenOne from "./src/screens/onbording/OnbordingScreenOne";
 
+//Fonts to use
+const loadFonts = () => {
+  return Font.loadAsync({
+    "mrt-xbold": require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+    "mrt-bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "mrt-medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  //Show App loading till fonts are loaded
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.error(err)}
+      />
+    );
+  }
+
   return <OnbordingScreenOne />;
 }
 
--- src/screens/onbording/OnbordingScreenOne.js
+++ src/screens/onbording/OnbordingScreenOne.js
@@ -3,15 +3,53 @@ import { StyleSheet, Text, View, Image } from "react-native";
 
 const OnbordingScreenOne = () => {
   return (
-    <View>
-      <Image
-        source={require("../../../assets/onbording/onboarding-image-one.png")}
-        resizeMode="contain"
-      />
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../../../assets/onbording/onboarding-image-one.png")}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.heading}>
+          Contra wireframe kit
+        </Text>
+        <Text style={styles.caption}>
+          Wireframe is still important for ideation. It will help you to quickly
+          test idea.
+        </Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+  },
+  imageContainer: {
+    marginTop: "20%",
+  },
+  textContainer: {
+    width: 300,
+    borderWidth: 1,
+    borderColor: "red",
+    marginTop: "25%",
+  },
+  heading: {
+    fontFamily: "mrt-xbold",
+    fontSize: 36,
+    textAlign: "center",
+    color: "#18191F",
+  },
+  caption: {
+    fontFamily: "mrt-medium",
+    fontSize: 21,
+    color: "#474A57",
+    textAlign: "center",
+  },
+});
 
 export default OnbordingScreenOne;
